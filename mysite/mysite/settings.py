629bce539cd1e5e0eacedb52832f031b270463cb
alderetebrian <alderetebrian@hotmail.com>
2021-02-03 02:09:00 -0300

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'r_4m^y*q)4avxq0ixg0ohb0kmdazi9j2_ee!+@io!yc6-d4d$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
